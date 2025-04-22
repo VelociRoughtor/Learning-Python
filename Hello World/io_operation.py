d4c631b2d82f0c06c0bc59681f6a60a4b3953904
YourUsername <mdmehedihasanrafy67865@gmail.com>
2025-04-22 23:39:46 +0600

    second commit

--- Hello World/io_operation.py
+++ Hello World/io_operation.py
@@ -1,4 +1,10 @@
 name = input('What is your name? ')
-print('Hi! ' + name)
 favorite_color = input('What is your favorite color? ')
 print(name + ' likes ' + favorite_color)
+
+
+birth_year = input('What is your birth year? ')
+print(type(birth_year))             # This will be a string
+age = 2025 - int(birth_year)
+print(type(age))                    # This will be an integer
+print(name + ' is ' + str(age) + ' years old.')
---
+++ Hello World/problem02.py
@@ -0,0 +1,3 @@
+weight = input('What is your weight(lb)? ')
+weight = int(weight) * 0.45359237
+print('Your weight(kg) is: ' + str(weight))
